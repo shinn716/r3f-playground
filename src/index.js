ba20c1746e9d9c4a9190c918cc48c7b66b6ced73
DELTA\JOHN.TC.TSAI <john.tc.tsai@deltaww.com>
2023-05-04 11:40:54 +0800

    practice css.

--- src/index.js
+++ src/index.js
@@ -4,16 +4,25 @@ import { Canvas } from "@react-three/fiber";
 import { Main } from "./Main";
 import { Html, useProgress } from "@react-three/drei";
 import { Suspense } from "react";
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
 
 function Loader() {
   const { progress } = useProgress();
   return <Html center>{Math.floor(progress)} % loaded</Html>;
 }
 
-createRoot(document.getElementById("root")).render(
-  <Canvas>
-    <Suspense fallback={<Loader />}>
-      <Main />
-    </Suspense>
-  </Canvas>
+ReactDOM.createRoot(document.querySelector("#root")).render(
+  <React.StrictMode>
+    <div className="dashboard">
+      <Canvas>
+        <Suspense fallback={<Loader />}>
+          <Main />
+        </Suspense>
+      </Canvas>
+    </div>
+    <div className="overlay" style={{ fontSize: 60 }}>
+      Apartment
+    </div>
+  </React.StrictMode>
 );
